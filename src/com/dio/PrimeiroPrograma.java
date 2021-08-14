8a5e947a096ca9b4066a9eaf6d2652fb2875b21f
Palomacord <84478374+Palomacord@users.noreply.github.com>
2021-08-14 11:31:36 -0300

    First Commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package com.dio;
+
+import com.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		/*int a =5;
+		int b = 10;*/
+		
+		Gato gato =   new Gato();
+		Livros livros = new Livros();
+      System.out.println(gato);
+	}
+
+}
+
+class Livros{
+	
+	private String nome;
+	private String tipo;
+	
+}
